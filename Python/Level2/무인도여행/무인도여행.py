edef96c1df2b531b8ab59b43e96c52ba0a1c5df5
Donghee <dongh94@naver.com>
2023-02-06 22:28:34 +0900

    무인도여행 BFS 완료

--- Python/Level2/무인도여행/무인도여행.py
+++ Python/Level2/무인도여행/무인도여행.py
@@ -2,28 +2,32 @@ from collections import deque
 
 dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
-def solution(maps):
 
+
+def solution(maps):
     answer = []
-    N = len(maps) # 행
-    M = len(maps[0]) # 열
-    visited = [[0]*M for _ in range(N)]
+    N = len(maps)  # 행
+    M = len(maps[0])  # 열
+    visited = [[0] * M for _ in range(N)]
     for r in range(N):
         for c in range(M):
             if maps[r][c] == "X": continue;
+            if visited[r][c] == 1: continue;
             ans = BFS(maps, r, c, N, M, visited)
             if ans != 0:
                 answer.append(ans)
+    if not answer:
+        answer = [-1]
     answer.sort()
     return answer
 
+
 def BFS(maps, r, c, N, M, visited):
     Q = deque([(r, c)])
-    num = 0
+    num = int(maps[r][c])
 
     while Q:
         sr, sc = Q.popleft()
-        if visited[sr][sc] == 1: continue;
         visited[sr][sc] = 1
         for d in range(4):
             nr = sr + dr[d]
@@ -32,4 +36,6 @@ def BFS(maps, r, c, N, M, visited):
             if maps[nr][nc] != "X" and visited[nr][nc] == 0:
                 visited[nr][nc] = 1
                 num += int(maps[nr][nc])
+                Q.append((nr, nc))
+
     return num
