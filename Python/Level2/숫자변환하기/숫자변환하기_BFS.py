113240d7a55cbe04b84c455f39582d07539ba312
Donghee <dongh94@naver.com>
2023-02-08 16:16:41 +0900

    숫자변환하기 BFS 완료

--- Python/Level2/숫자변환하기/숫자변환하기_BFS.py
+++ Python/Level2/숫자변환하기/숫자변환하기_BFS.py
@@ -6,23 +6,25 @@ def solution(x, y, n):
     answer = BFS(x, y, n)
     return answer
 
-
 def BFS(x, y, n):
     Q = deque()
     Q.append((x, 0))
-
+    visited = [0]*1000001
+    visited[x] = 1
     while Q:
         node, cnt = Q.popleft()
         if node == y: return cnt
-        if node + n > y: return -1
-
-        for i in [n, 2, 3]:
-            if i == n:
-                if node + i > y: continue;
+        if node > 10000000: return -1
+        for i in range(1, 4):
+            if i == 1:
+                i = n
+                if node + i > 1000000 or visited[node + n]: continue;
                 new_node = node + i
+                visited[new_node] = 1
                 Q.append((new_node, cnt + 1))
             else:
-                if node * i > y: continue;
+                if node * i > 1000000 or visited[node * i]: continue;
                 new_node = node * i
+                visited[new_node] = 1
                 Q.append((new_node, cnt + 1))
     return -1
