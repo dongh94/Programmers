a3fc876aa4d034fc33f7872b329e3915341b50b8
Donghee <dongh94@naver.com>
2023-02-08 16:22:47 +0900

    숫자변환하기 DP 완료 및 풀이 기록

--- Python/Level2/숫자변환하기/숫자변환하기_BFS.py
+++ Python/Level2/숫자변환하기/숫자변환하기_BFS.py
@@ -2,7 +2,6 @@ from collections import deque
 
 
 def solution(x, y, n):
-    answer = 0
     answer = BFS(x, y, n)
     return answer
 
--- Python/Level2/숫자변환하기/숫자변환하기_DP.py
+++ Python/Level2/숫자변환하기/숫자변환하기_DP.py
@@ -1,17 +1,17 @@
 def solution(x, y, n):
-    dp = [float('inf')]*(y+1)
+    INF = int(1e9)
+    dp = [INF]*(y+1)
     dp[x] = 0
     for i in range(x, y+1):
-        if dp[i] == float('inf'):
-            continue
-        if i+n<=y:
+        if dp[i] == INF: continue;
+        if i + n <= y:
             dp[i+n] = min(dp[i+n], dp[i]+1)
-        if i*2<=y:
+        if i * 2 <= y:
             dp[i*2] = min(dp[i*2], dp[i]+1)
-        if i*3<=y:
+        if i * 3 <= y:
             dp[i*3] = min(dp[i*3], dp[i]+1)
 
-    if dp[y]==float('inf'):
+    if dp[y]==INF:
         return -1
     else:
         return dp[y]
