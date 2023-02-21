4b5eabd48fb9535664bc7b390061fe6ed3adcf73
Donghee <dongh94@naver.com>
2023-02-21 22:58:23 +0900

    둘만의 암호 풀이완료 (설계 순서그대로)

--- Python/Level1/둘만의암호/둘만의암호.py
+++ Python/Level1/둘만의암호/둘만의암호.py
@@ -1,25 +1,20 @@
 def solution(s, skip, index):
     answer = ''
     for st in s:
-        cnt = index - 1
-        node = ord(st) - 97 + 1  # a => 0 다음거니까 + 1
-        if node == 26:
-            node = 0
+        cnt = index
+        node = ord(st) - 97  # a => 0
 
         while cnt:
+            if node == 25:
+                node = 0
+            else:
+                node += 1
+
             for sk in skip:
                 skipnode = ord(sk) - 97
-                if node == skipnode and node < 25:
-                    node += 1
-                    break
-                elif node == skipnode and node == 25:
-                    node = 0
+                if node == skipnode:
                     break
             else:
-                if node < 25:
-                    node += 1
-                else:
-                    node = 0
                 cnt -= 1
         answer += chr(node + 97)
     return answer
