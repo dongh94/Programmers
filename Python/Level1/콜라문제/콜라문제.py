78142c6275b65d9c2e7c6beed11a539ef98386fa
Donghee <dongh94@naver.com>
2023-02-28 20:25:19 +0900

    콜라문제 풀이완료 (수학)

--- Python/Level1/콜라문제/콜라문제.py
+++ Python/Level1/콜라문제/콜라문제.py
@@ -2,8 +2,8 @@ def solution(a, b, n):
     answer = 0
 
     while n >= a:
-        n = n - a + b
-        answer += b
-
+        x, y = divmod(n, a)
+        n = n - (a * x) + (b * x)
+        answer += b * x
 
     return answer
